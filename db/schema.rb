f6501d4918d4bee7166f050d969e11495c220354
jamalparit <jamalparit@outlook.com>
2014-10-28 05:25:34 +0800

    Revert "Finish log in/log out"
    This reverts commit 3e17e8c78a5c0c2ff5d492af51a832341e642b54.
    
    	modified:   app/assets/javascripts/application.js
    	deleted:    app/assets/javascripts/sessions.js.coffee
    	modified:   app/assets/stylesheets/custom.css.scss
    	deleted:    app/assets/stylesheets/sessions.css.scss
    	modified:   app/controllers/application_controller.rb
    	deleted:    app/controllers/sessions_controller.rb
    	modified:   app/controllers/users_controller.rb
    	deleted:    app/helpers/sessions_helper.rb
    	modified:   app/models/user.rb
    	modified:   app/views/layouts/_header.html.erb
    	deleted:    app/views/sessions/new.html.erb
    	modified:   config/routes.rb
    	deleted:    db/migrate/20141027125043_add_remember_digest_to_users.rb
    	modified:   db/schema.rb
    	deleted:    test/controllers/sessions_controller_test.rb
    	deleted:    test/helpers/sessions_helper_test.rb
    	deleted:    test/integration/users_login_test.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027125043) do
+ActiveRecord::Schema.define(version: 20141026081143) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20141027125043) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
-    t.string   "remember_digest"
   end
 
 end
